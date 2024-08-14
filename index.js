527e5f15b6728c67fc42ac082998b8fb30b745d1
Mrsapo-ai <g2meoy3a@gmail.com>
2024-08-14 19:24:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -68,35 +68,11 @@ client.once('ready', () => {
     updateStatusAndSendMessages();
   }, 10000);
 });
-
-login();
 const { joinVoiceChannel } = require('@discordjs/voice');
-
-client.once('ready', () => {
-
-    const sesKanaliId = '1265428206673203322';
-
-    const sesKanali = client.channels.cache.get(sesKanaliId);
-
-    if (sesKanali) {
-        if (sesKanali.type === 2) {
-
-            const botSesKanali = sesKanali.guild.members.me.voice.channel;
-
-            if (botSesKanali && botSesKanali.id === sesKanaliId) {
-                console.log('Bot zaten ses kanalına bağlı!');
-            } else {
-                joinVoiceChannel({
-                    channelId: sesKanali.id,
-                    guildId: sesKanali.guild.id,
-                    adapterCreator: sesKanali.guild.voiceAdapterCreator,
-                });
-                console.log('Ses kanalına katılındı!');
-            }
-        } else {
-            console.error('Belirtilen kanal bir ses kanalı değil!');
-        }
-    } else {
-        console.error('Belirtilen ses kanalı bulunamadı!');
-    }
+ client.on('ready', () => { 
+  joinVoiceChannel({
+channelId: "1266757444672028752",
+guildId: "1258421889269760080",       
+adapterCreator: client.guilds.cache.get("Sunucu ID").voiceAdapterCreator
+    });
 });
